69c4a5f9916536af6cc3b995e2eeef894fa9c33b
Taguchi-ivi <k.taguchi@ivisystem.com>
2023-04-27 00:33:51 +0900

    useState onClick event study

--- src/App.tsx
+++ src/App.tsx
@@ -43,6 +43,45 @@ function App() {
     num: 123
   }
 
+  let [val, setVal] = useState("")
+  const [cnt, setCnt] = useState(0)
+  const countUp = () => {
+    setCnt(cnt + 1)
+  }
+  const countDown = () => {
+    setCnt(cnt - 1)
+  }
+
+  const personObj = {name: "Tom", age: 18}
+  const [person, setPerson] = useState(personObj)
+
+  const changeName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // setPerson({ name: e.target.value, age: person.age })
+    setPerson({ ...person, name: e.target.value })
+  }
+
+  const changeAge = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const age = parseInt(e.target.value)
+    // setPerson({ name: person.name, age: age })
+    setPerson({ ...person, age: age })
+  }
+
+  const resetPerson = () => {
+    setPerson({ name: "", age: 0 })
+  }
+
+  const numArray = [1 , 2, 3, 4, 5]
+  const [nums, setNums] = useState(numArray)
+
+  const shuffle = () => {
+    const newNums = [...nums];
+    const value: number | undefined = newNums.pop();
+    if(value !== undefined) {
+      newNums.unshift(value);
+      setNums(newNums);
+    }
+  }
+
   return (
     // <div className="App">
     //   <header className="App-header">
@@ -84,6 +123,35 @@ function App() {
           obj={{ name: 'taguchi', age: 20 }}
         />
       </Container>
+      <input
+        type="text"
+        onChange={(e) => {
+          setVal(e.target.value)
+        }}
+      /> = {val}
+      <p>現在のカウント数：{cnt}</p>
+      <button
+        onClick={countUp}
+      >
+        +
+      </button>
+      <button
+        onClick={countDown}
+      >
+        -
+      </button>
+
+      <h3>Name:{person.name}</h3>
+      <h3>Age:{person.age}</h3>
+      <input type="text" value={person.name} onChange={changeName}/>
+      <input type="number" value={person.age} onChange={changeAge}/>
+      <div>
+        <button onClick={resetPerson}>リセット</button>
+      </div>
+      <h1>
+        {nums}
+      </h1>
+      <button onClick={shuffle}>shuffle</button>
     </div>
   );
 }
