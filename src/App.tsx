12deacb80a0fcfec4f2a761a77f551d5aa0b92ea
Taguchi-ivi <k.taguchi@ivisystem.com>
2023-04-27 23:39:45 +0900

    chakra-ui && react-icons

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 // import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import { ChakraProvider } from '@chakra-ui/react'
 import { useState } from 'react'
 import { MyList } from './components/List'
 import Expression from './components/Expression'
@@ -181,7 +182,9 @@ function App() {
         }
       </ul>
       <div>
-        <MyTodo />
+        <ChakraProvider>
+          <MyTodo />
+        </ChakraProvider>
       </div>
     </div>
   );
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,3 +1,6 @@
+import { VStack, StackDivider, HStack, IconButton, Text } from '@chakra-ui/react'
+import { VscCheck } from 'react-icons/vsc'
+
 interface Todo {
     id: number;
     content: string;
@@ -14,17 +17,34 @@ const TodoList = ({todos, deleteTodo}: TodoListProps) => {
         deleteTodo(id)
     }
     return (
-        <div>
+        // color={{sm, md, lg, xl, '2xl'}} メディアクエリ
+        <VStack
+            divider={<StackDivider /> }
+            borderColor="blackAlpha.100"
+            color={{sm: 'red.600', md: 'blue.600', lg: 'green.600', xl: 'yellow.600'}}
+            borderWidth="1px"
+            borderRadius="3px"
+            p={5}
+            alignItems="start"
+        >
             {todos.map((todo: Todo) => {
                 return (
-                    <div key={todo.id}>
-                        <button onClick={() => complete(todo.id)}>完了</button>
-                        <span>{todo.content}</span>
-                    </div>
+                    <HStack key={todo.id} spacing="5">
+                        <IconButton
+                            onClick={() => complete(todo.id)}
+                            icon={<VscCheck />}
+                            aria-label={`Complete "${todo.content}"`}
+                            isRound
+                            bgColor="chan.100"
+                        >
+                            完了
+                        </IconButton>
+                        <Text>{todo.content}</Text>
+                    </HStack>
                 )
             })}
-        </div>
+        </VStack>
     )
-}
 
+}
 export default TodoList
