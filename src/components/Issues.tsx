e743440e0aac617cbf5a552c0fbacc5843bb94c6
Taguchi-ivi <k.taguchi@ivisystem.com>
2023-05-04 12:19:30 +0900

    issues UI fix #7

--- src/components/Issues.tsx
+++ src/components/Issues.tsx
@@ -3,7 +3,12 @@ import {gql, useQuery} from '@apollo/client';
 import { useParams } from 'react-router-dom';
 import { Helmet } from "react-helmet-async";
 import Loading from './Atoms/Loading'
-import { Box, Button } from '@chakra-ui/react';
+import {
+    Box, Button, Flex, Heading, Text,
+    Card, CardBody, Stack, Divider
+} from '@chakra-ui/react';
+// import { FiExternalLink } from "react-icons/fi";
+import { FaGithub } from 'react-icons/fa';
 
 // type Repository = {
 //     name: string;
@@ -63,18 +68,37 @@ const Issues: React.FC = () => {
             </Helmet>
             {!id && (<h3>正しいURLを指定してください</h3>)}
             {data && (
-                <Box>
-                    <Button onClick={PageBack}>←back to search</Button>
-                    <span>Latest issues</span>
-                    <span>on {data.node.owner.login}/{data.node.name}</span>
-                    <ul>
-                        {data.node.issues.edges.map(( edge: any, index: number ) => (
-                            <li key={index}>
-                                <a href={edge.node.url} target="_blank" rel="noopener noreferrer">{edge.node.title}</a>
-                            </li>
-                        ))}
-                    </ul>
-                </Box>
+                <Card>
+                    <CardBody>
+                        <Button onClick={PageBack}>← back to search</Button>
+                        <Flex my="8">
+                            <Heading size='sm' mr="2">
+                                Latest issues
+                            </Heading>
+                            <Text fontSize='xs' color="gray.400">
+                                on {data.node.owner.login}/{data.node.name}
+                            </Text>
+                        </Flex>
+                        <Stack>
+                            {data.node.issues.edges.map(( edge: any, index: number ) => (
+                                <Box key={index}>
+                                    <Box p="3">
+                                        <a href={edge.node.url} target="_blank" rel="noopener noreferrer">
+                                            <Flex>
+                                                <Text fontSize='md' color="gray.600" mr="3">
+                                                    {edge.node.title}
+                                                </Text>
+                                                {/* <FiExternalLink /> */}
+                                                <FaGithub />
+                                            </Flex>
+                                        </a>
+                                    </Box>
+                                    <Divider />
+                                </Box>
+                            ))}
+                        </Stack>
+                    </CardBody>
+                </Card>
             )}
         </>
     )
